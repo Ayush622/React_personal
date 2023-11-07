b7a1f4cf9dd930f697f4f5eb201ef6ab85ce4108
Ayush622 <ayush7272622@gmail.com>
2023-11-07 16:20:19 +0530

    Ep 02 ends

--- App.js
+++ App.js
@@ -1,6 +1,17 @@
-import React from 'react';
-import  ReactDOM  from 'react-dom/client';
-const heading = React.createElement("h1", {id:"heading"}, "Hello from react");
-const rootElement = document.getElementById('root');
-const root = ReactDOM.createRoot(rootElement);
-root.render(heading)
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const Title = () => {
+  return (<h1> Ayush</h1>)
+};
+
+const HeadingComponent = () => (
+  <div>
+    <Title />
+    <h1>Ayush is Raja</h1>
+  </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<HeadingComponent />);
